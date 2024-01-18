456b13054c78a2f9a49cd75cda393a2bcd2f377e
notauhuynh <142616016+notauhuynh@users.noreply.github.com>
2024-01-17 20:15:17 -0800

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,69 +1,10 @@
-
-
 public class Main {
 	
 	public static void main(final String[] args) {
 		PigLatin sim = new PigLatin();
         	sim.tester();
-    String[] lines = {"beast", "dough", "happy", "question", "star", "three", "eagle", "try"};
-    // String phrase = "With heads bared we stand In tribute to thee Our Alma-Mater Lowell All true to thee we'll be Unfurled red and white None shall thee decry They name we love Oh Lowell High";
-
-  
-    // stringDectector(phrase);
-  System.out.println("there are " + lines.length + " lines");
-  for (int i = 0; i < lines.length; i++)
-  {
-    System.out.println(pigLatin(lines[i]));
-  }
-  // for(int i = 0; i < list.size(); i++)
-  // {
-  //   System.out.print(pigLatin(list.get(i)) + " ");
-    
-  // }
 	}
-	public static int findFirstVowel(String word)
-  //precondition: sWord is a valid String of length greater than 0.
-  //postcondition: returns the position of the first vowel in sWord.  If there are no vowels, returns -1
-{
-  
-  for (int i = 0; i < word.length(); i++)
-    if (word.charAt(i) == 'a' || word.charAt(i) == 'e' ||
-      word.charAt(i) == 'i' || word.charAt(i) == 'o' || word.charAt(i) == 'u' ||
-      word.charAt(i) == 'A' || word.charAt(i) == 'E' ||
-      word.charAt(i) == 'I' || word.charAt(i) == 'O' || word.charAt(i) == 'U')
-      return i;
-  return -1;
-}
-
-public static String pigLatin(String sWord)
-  //precondition: sWord is a valid String of length greater than 0
-  //postcondition: returns the pig latin equivalent of sWord
-{
-  if (findFirstVowel(sWord) == -1)
-  {
-    return sWord + "ay";
-  } else if (findFirstVowel(sWord) == 0) {
-    return sWord + "way";
-  } else if (sWord.substring(0, 2).equals("qu")) {
-    return sWord.substring(2) + "quay";
-  } else if (findFirstVowel(sWord) > 0) { 
-    return sWord.substring(findFirstVowel(sWord)) + sWord.substring(0, findFirstVowel(sWord)) + "ay";
-  } else {
-    return "ERROR!";
-  }
-}
-// public static void stringDectector(String sWord)
-// {
-//   int indexPos = 0;
-//   for(int i = 0; i < sWord.length(); i++){
-//     if(sWord.substring(i, i+1).equals(" ")){
-//       list.add(sWord.substring(indexPos, i));
-//       indexPos = i + 1;
-//       System.out.println(list);
-//       System.out.println(list.size());
-//     }
-//   }
-// }
+	
 
 
 }
