ed991852dd07f49a4ed87564d53a93e5b47bd6e5
notauhuynh <142616016+notauhuynh@users.noreply.github.com>
2024-01-17 20:20:13 -0800

    Update PigLatin.java

--- src/main/java/PigLatin.java
+++ src/main/java/PigLatin.java
@@ -34,7 +34,7 @@ public class PigLatin {
 	        System.out.println(pigLatin(lines[i]));
 	    }
     }
-    public int findFirstVowel(String sWord) {
+    public int findFirstVowel(String word) {
         for (int i = 0; i < word.length(); i++)
     if (word.charAt(i) == 'a' || word.charAt(i) == 'e' ||
       word.charAt(i) == 'i' || word.charAt(i) == 'o' || word.charAt(i) == 'u' ||
